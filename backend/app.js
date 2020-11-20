1b0e2a524009825ced4b770db1cbb38c92c125f7
Sari <sariguci@Saris-MacBook-Pro.local>
2020-11-20 16:24:24 +0100

    added hashed password

--- backend/app.js
+++ backend/app.js
@@ -1,33 +1,53 @@
 const express = require("express");
 const app = express();
-var bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
 
-var jsonParser = bodyParser.json();
+//var jsonParser = bodyParser.json();
+
+app.use(express.json());
 
 const PORT = 5000;
 
 const db = require("./db");
 const collection = "users";
 
-app.post('/createAccount', jsonParser, (req, res) => {
+app.get('/', (req, res) => {
+  db.getDB().collection(collection).find().toArray((error, collection) => {
+    console.log(collection);
+  });
+});
 
-  let newUser = {
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    email: req.body.email,
-    password: req.body.password
+app.post('/createAccount', async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    let newUser = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      password: hashedPassword
+    }
+
+
+    db.getDB().collection(collection).insertOne(newUser, (error, result) => {
+      console.log(result.ops[0]);
+      if (result.insertedCount === 1) {
+        console.log("new user");
+      } else {
+        throw new Error(error);
+      }
+    });
+
+  } catch{
+    res.status(500).send();
   }
 
-  console.log(newUser);
-  db.getDB().collection(collection).insertOne(newUser, (error, result) => {
-    console.log(result.ops[0]);
-    if (result.insertedCount === 1) {
-      console.log("new user");
-    } else {
-      throw new Error(error);
-    }
-  });
 
+})
+
+app.post('/login', async (req, res) => {
+  //bcrypt compare to compare hashed password and input
 })
 
 app.listen(PORT, function () {
